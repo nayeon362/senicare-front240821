61659b72a5629092efeaffee0f055c4ba1850900
nanayeon <tires0301@gmail.com>
2024-08-22 12:35:18 +0900

    12:50

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import './style.css';
 
-export default function InputBox() {
+interface Props {
+    label: string;
+    type: 'text' | 'password';
+    placeholder: string;
+    buttonName?: string;
+}
+export default function InputBox({ label, type, placeholder, buttonName }: Props) {
     return (
-        <div id="auth-number-box" className="input-box">
-            <div className="label">인증번호</div>
+        <div className="input-box">
+            <div className="label">{label}</div>
             <div className="input-area">
-                <input id="auth-number" type="text" placeholder="인증번호 4자리를 입력해주세요." />
-                <div id="auth-number-button" className="input-button disable">인증 확인</div>
+                <input type={type} placeholder={placeholder} />
+                {buttonName && <div className="input-button disable">{buttonName}</div>}
             </div>
-            <div id="auth-number-message" className="message"></div>
+            <div className="message"></div>
         </div>
-
     )
 }
+
