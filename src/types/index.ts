5c689d034e3edaa15bae4d978389096b20895772
nanayeon <tires0301@gmail.com>
2024-09-26 17:28:58 +0900

    5:50

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,9 @@
 import SignInUser from "./sign-in-user.interface";
+import Customer from "./customer.interface";
 import Tool from "./tool.interface";
 
 export type {
     Tool,
+    Customer,
     SignInUser
 }
